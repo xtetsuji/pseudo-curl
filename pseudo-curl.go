eaf1d6cb4489448460b9472951978ab1565c939a
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2020-03-03 13:38:41 +0900

    remove os.Exit(1) before log.Fatal() because log.Fatal() invoke it

--- pseudo-curl.go
+++ pseudo-curl.go
@@ -19,22 +19,18 @@ func main() {
 	}
 	if len(url) == 0 {
 		log.Fatal("first argument is required")
-		os.Exit(1)
 	}
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	res, geterr := http.Get(url)
 	if geterr != nil {
 		log.Fatal(geterr)
-		os.Exit(1)
 	}
 	if res.StatusCode < 200 || res.StatusCode >= 300 {
 		log.Fatal("HTTP Response is not valid: " + res.Status)
-		os.Exit(1)
 	}
 	body, reserr := ioutil.ReadAll(res.Body)
 	if reserr != nil {
 		log.Fatal(reserr)
-		os.Exit(1)
 	}
 	fmt.Println(string(body))
 	os.Exit(0)
